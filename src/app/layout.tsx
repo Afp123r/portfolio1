65016e75538c2b30c1ca6c694947f7a77c81f72c
HenryNeoh <Afp123r@users.noreply.github.com>
2026-09-18 13:17:52 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,11 @@ export const metadata: Metadata = {/*website name change here*/
     },
   },
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/images/logo.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/logo.png', sizes: '192x192', type: 'image/png' },
+      { url: '/images/logo.png', sizes: '512x512', type: 'image/png' },
+    ],
     apple: '/images/logo.png',
   },
   manifest: '/manifest.json',
